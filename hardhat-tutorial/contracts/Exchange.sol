017055e273aca87d90f209a476b8df90eec28c31
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2022-10-14 12:49:37 +0530

    Initial commit

--- hardhat-tutorial/contracts/Exchange.sol
+++ hardhat-tutorial/contracts/Exchange.sol
@@ -1,9 +1,15 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-contract Exchange is ERC20{
-     
 
+contract Exchange is ERC20 {
+    address public cryptoDevTokenAddress;
 
-
+    constructor(address _CryptoDevtoken) ERC20("CryptoDev LP Token", "CDLP") {
+        require(
+            _CryptoDevtoken != address(0),
+            "Token address passed is a null address"
+        );
+        cryptoDevTokenAddress = _CryptoDevtoken;
+    }
 }
